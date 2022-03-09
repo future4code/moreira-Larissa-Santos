db734f95179d85663742d3e6e67bd4dfa27dfabd
Larissa97-fir <larissafirmino097@gmail.com>
2022-03-09 09:38:26 -0300

    Labeddit

---
+++ modulo3/labeddit/labEddit/src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function App() {
+  return (
+    <div className="App">
+      <headeimport { Router } from "./Routes/Router";
+
+function App() {
+  return (
+   
+
+      <Router />
+    
+
+  );
+}
+
+export default App;
