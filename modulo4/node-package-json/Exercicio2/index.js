9f14adeadeb7e49f155fa46209c106573feb7eb1
Larissa97-fir <larissafirmino097@gmail.com>
2022-03-22 21:09:31 -0300

    Exercicios-de-Node-e-package.json

---
+++ modulo4/node-package-json/Exercicio2/index.js
@@ -0,0 +1,22 @@
+//Exercício 2
+
+const valor1 = Number(process.argv[3])
+const valor2 = Number(process.argv[4])
+
+
+const operacao = process.argv[2]
+
+switch(operacao){
+	case "soma":
+        console.log(valor1 + valor2)
+		break;
+	case "subt":
+		console.log(valor1 - valor2)
+		break;
+    case "mult":
+		console.log(valor1 * valor2)
+		break;
+    case "div":
+		console.log(valor1 / valor2)
+		break;
+}
